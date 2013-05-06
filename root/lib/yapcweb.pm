51a4e5e99e1683930d0e6496b84322db13a0089a
Felipe da Veiga Leprevost <leprevost@cpan.org>
2013-05-06 20:56:23 -0300

    loading files

--- root/lib/yapcweb.pm
+++ root/lib/yapcweb.pm
@@ -1,7 +1,17 @@
 package yapcweb;
+
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
+
+prefix '/';
+
+hook before_template => sub {
+	my $tokens = shift;
+	$tokens->{uri_base} = request->base->path eq '/' ? '' : request->base->path;	
+	$tokens->{'index'}  = uri_for('/');
+};
+
 
 get '/' => sub {
     template 'index';
