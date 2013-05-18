cd429ad95f3caf22753e31b9bca27fca47aacf2f
Felipe da Veiga Leprevost <leprevost@cpan.org>
2013-05-18 14:44:57 -0300

    adicionando envio de emails

--- lib/yapcweb.pm
+++ lib/yapcweb.pm
@@ -2,6 +2,7 @@ package yapcweb;
 
 use Dancer ':syntax';
 use Dancer::Plugin::I18N;
+use Dancer::Plugin::Email;
 use HTTP::AcceptLanguage;
 
 our $VERSION = '0.1';
@@ -32,4 +33,56 @@ get '/:opt_lang' => sub {
 	template 'index';
 };
 
+post '/' => sub {
+
+	my %talk;
+
+	$talk{name}		= params->{nomePalestrante};
+	$talk{email}	= params->{emailPalestrante};
+	$talk{title}	= params->{tituloPalestrante};
+	$talk{abstract}	= params->{resumoPalestrante};
+
+	email {
+		from 	=> 'YAPC Brasil',
+		to   	=> 'yapc-curitiba@googlegroups.com',
+		subject	=> 'Nova Palestra',
+		body	=> "Nome: $talk{name}\nE-mail: $talk{email}\nTítulo: $talk{title}\nResumo: $talk{abstract}\n",
+	};
+
+    email {
+        from    => 'YAPC Brasil',
+        to      => "$talk{email}",
+        subject => 'Palestra Enviada',
+        body    => "Olá $talk{name},\n sua palestra entitulada: $talk{abstract} foi enviada com sucesso.\nIremos avaliar sua proposta e logo retornaremos uma resposta.",
+    };
+
+	redirect '/';	
+};
+
 true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
