80561c675c347dfec6fcb03646f1f25f3c224905
Felipe da Veiga Leprevost <leprevost@cpan.org>
2013-05-21 20:35:33 -0300

    arrumando o envio de palestras

--- lib/yapcweb.pm
+++ lib/yapcweb.pm
@@ -18,25 +18,30 @@ hook before_template => sub {
 
 get '/' => sub {
 
+	# gets the language of preference from http request header
+	# and sets the session variable
 	my $accept_language = HTTP::AcceptLanguage->new(request->accept_language);
-	session user_lang => $accept_language;
+	session user_lang => $accept_language->languages;
 	
 	template 'index';
 };
 
-
 get '/:opt_lang' => sub {
 
+	# changes the prefered language by demand
 	my $opt_lang = param('opt_lang');
 	session user_lang => $opt_lang;
 
 	template 'index';
 };
 
-post '/' => sub {
+post '/:opt_lang?' => sub {
 
+	my $opt_lang = ( param('opt_lang') || 'empty' );
+
+	# verify if the "captcha" in the form is correct
 	if (params->{verificaPalestrante} eq '5') {
-		
+
 		my %talk;
 
 		$talk{name}		= params->{nomePalestrante};
@@ -53,17 +58,30 @@ post '/' => sub {
 			subject	=> 'Nova Palestra',
 			body	=> "Nome: $talk{name}\nE-mail: $talk{email}\nDia de preferência: $talk{dia}\nDuração: $talk{tempo}\nTítulo: $talk{title}\nResumo: $talk{abstract}\n",
 		};
-	
-		# para o palestrante
-	    email {
-	        from    => 'mailer@hexabio.com.br',
-	        to      => "$talk{email}",
-	        subject => 'Palestra Enviada',
-	        body    => "Olá $talk{name}!\nSua palestra entitulada: \'$talk{title}\' foi enviada com sucesso.\n\nIremos avaliar sua proposta e logo retornaremos uma resposta.",
-	    };
+
+		if ($opt_lang eq 'pt') {
+
+			# para o palestrante brasileiro
+		    email {
+		        from    => 'mailer@hexabio.com.br',
+		        to      => "$talk{email}",
+		        subject => 'Palestra Enviada',
+		        body    => "Olá $talk{name}!\nSua palestra entitulada: \'$talk{title}\' foi enviada com sucesso.\n\nIremos avaliar sua proposta e logo retornaremos uma resposta.",
+		    };
+	        
+		} else {
+			
+			# para o palestrante de fora
+			email {
+				from    => 'mailer@hexabio.com.br',
+				to      => "$talk{email}",
+				subject => 'Talk submitted',
+				body    => "Hello $talk{name}!\nYour talk entitled: \'$talk{title}\' was successfully submitted.\n\nAfter evaluation we will return to you with an answer.\n",
+			};
+		}
 	}
 
-	return redirect '/';
+	return redirect "/";
 
 };
 
